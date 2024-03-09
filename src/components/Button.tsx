fb1c6fdd6eed04f02dbc8dccd211243f2d45ad95
revdev <mail@revdev.me>
2024-03-09 13:58:56 +0100

    some improvements

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,27 @@
+import { motion, type MotionProps, type TargetAndTransition } from 'framer-motion';
 import { observer } from 'mobx-react';
 import React, { type ButtonHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
-const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = observer(({ ...rest }) => {
-  return <_Button {...rest} />;
+const Button: React.FC<MotionProps & ButtonHTMLAttributes<HTMLButtonElement>> = observer(({ ...rest }) => {
+  return <_Button whileHover={buttonHover} {...rest} />;
 });
 
-const _Button = styled.button`
+const _Button = styled(motion.button)`
   border: 1px solid rgb(48, 54, 61);
   border-radius: 4px;
   padding: 5px 12px;
   background-color: transparent;
   align-self: self-start;
+
+  :hover {
+    cursor: pointer;
+  }
 `;
+
+const buttonHover: TargetAndTransition = {
+  border: '1px solid rgb(48, 54, 61)',
+  backgroundColor: 'rgb(48, 54, 61)'
+};
 
 export { Button };
