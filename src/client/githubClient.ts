ecef9b51b0a6e7e6880fc484baf29eec1a2fccd2
revdev <mail@revdev.me>
2024-03-10 13:11:03 +0100

    sort imports

--- src/client/githubClient.ts
+++ src/client/githubClient.ts
@@ -1,6 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
-import type { GhResponse } from 'type/github';
-import type { Repository } from 'util/github';
+
+import type { GhResponse } from '~type/github';
+import type { Repository } from '~util/github';
 
 class GithubClient {
   constructor(token: string, repository: Repository) {
