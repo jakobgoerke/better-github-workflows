f376cf5a78acec449375a033fa5dc3db81475630
revdev <mail@revdev.me>
2024-07-15 22:48:17 +0200

    sort workflow list by name

--- src/components/workflow/WorkflowList.tsx
+++ src/components/workflow/WorkflowList.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 import { Link } from '~components/common';
 import { useStore } from '~hook/useStore';
+import type { Workflow } from '~type/github';
 import { getWorkflowFileNameFromPath } from '~util/github';
 
 const WorkflowList: React.FC = observer(() => {
@@ -14,9 +15,13 @@ const WorkflowList: React.FC = observer(() => {
     return `https://github.com/${appStore.repository.owner}/${appStore.repository.name}/actions/workflows/${file}`;
   };
 
+  const sortByName = (a: Workflow, b: Workflow) => {
+    return a.name.localeCompare(b.name);
+  }
+
   return (
     <Wrapper>
-      {appStore.filteredWorkflows.map((workflow) => (
+      {appStore.filteredWorkflows.sort(sortByName).map((workflow) => (
         <Link data-testid="workflow-link" key={workflow.id} href={buildWorkflowLink(workflow.path)}>
           {workflow.name}
         </Link>
